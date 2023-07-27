2ab81e359884857a87bdfb83cc5a93e5c44857e5
kineticstorytelling <kineticstorytelling@gmail.com>
2023-07-26 21:39:34 -0400

    'removed font awesome extra folder'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 
-import viteLogo from '/vite.svg'
 import './App.css'
 // testing 
 function App() {
@@ -9,9 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
